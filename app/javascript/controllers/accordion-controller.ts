e8595a10353341cc9c4e962dd6104da27e0ac414
Lewis Monteith <lewismonteith@gmail.com>
2024-02-08 21:03:14 +0000

    Add a hacky way of keeping accordions open

--- app/javascript/controllers/accordion-controller.ts
+++ app/javascript/controllers/accordion-controller.ts
@@ -1,7 +1,25 @@
 import { Controller } from '@hotwired/stimulus';
 
+let accordions: string[] = [];
+
 export default class extends Controller<HTMLDivElement> {
+  public connect(): void {
+    if (accordions.includes(this.label)) {
+      this.element.classList.add('open');
+    }
+  }
+
   public toggle(): void {
-    this.element.classList.toggle('open');
+    if (this.element.classList.contains('open')) {
+      this.element.classList.remove('open');
+      accordions = accordions.filter(a => a === this.label);
+    } else {
+      this.element.classList.add('open');
+      accordions.push(this.label);
+    }
+  }
+
+  private get label(): string {
+    return this.element.getAttribute('data-label')!;
   }
 }
