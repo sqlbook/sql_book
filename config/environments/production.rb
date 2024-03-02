04b1d60b617da924b12e5980773aa1c0f640d346
Lewis Monteith <lewismonteith@gmail.com>
2024-03-02 16:04:48 +0000

    Disable request forgery for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,6 +34,8 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.asset_host = 'https://cdn.sqlbook.com'
 
+  config.action_cable.disable_request_forgery_protection = true
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
