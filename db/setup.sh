54557d48704796bae2dbaa9d4b7021bbf6af79de
Lewis Monteith <lewismonteith@gmail.com>
2025-04-11 20:02:23 +0100

    Use env vars from ci

--- db/setup.sh
+++ db/setup.sh
@@ -3,7 +3,11 @@
 # Usage
 # ./db/setup.sh <environment>
 
-psql -U postgres -c "CREATE ROLE sql_book_readonly WITH LOGIN PASSWORD 'password';" || true
-psql -U postgres -d sql_book_events_$1 -c "GRANT SELECT ON clicks TO sql_book_readonly;"
-psql -U postgres -d sql_book_events_$1 -c "GRANT SELECT ON page_views TO sql_book_readonly;"
-psql -U postgres -d sql_book_events_$1 -c "GRANT SELECT ON sessions TO sql_book_readonly;"
+export PGUSER="${DATABASE_USER:-postgres}"
+export PGPASSWORD="${DATABASE_PASSWORD:-postgres}"
+export PGHOST="${DATABASE_HOST:-localhost}"
+
+psql -c "CREATE ROLE sql_book_readonly WITH LOGIN PASSWORD 'password';" || true
+psql -d sql_book_events_$1 -c "GRANT SELECT ON clicks TO sql_book_readonly;"
+psql -d sql_book_events_$1 -c "GRANT SELECT ON page_views TO sql_book_readonly;"
+psql -d sql_book_events_$1 -c "GRANT SELECT ON sessions TO sql_book_readonly;"
