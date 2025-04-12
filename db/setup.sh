639669dbcab5567c612cc73eeb99b3d2fbabfa58
Lewis Monteith <lewismonteith@gmail.com>
2025-04-12 21:12:10 +0100

    Add accessories

--- db/setup.sh
+++ db/setup.sh
@@ -3,9 +3,9 @@
 # Usage
 # ./db/setup.sh <environment>
 
-export PGUSER="${DATABASE_USER:-postgres}"
-export PGPASSWORD="${DATABASE_PASSWORD:-postgres}"
-export PGHOST="${DATABASE_HOST:-localhost}"
+export PGUSER="${POSTGRES_USER:-postgres}"
+export PGPASSWORD="${POSTGRES_PASSWORD:-postgres}"
+export PGHOST="${POSTGRES_HOST:-localhost}"
 
 psql -c "CREATE ROLE sql_book_readonly WITH LOGIN PASSWORD 'password';" || true
 psql -d sql_book_events_$1 -c "GRANT SELECT ON clicks TO sql_book_readonly;"
