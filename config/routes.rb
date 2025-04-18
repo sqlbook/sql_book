6a17768da62754cb14a6cd39e88ec3fccd78dfaf
Lewis Monteith <lewismonteith@gmail.com>
2025-04-18 10:19:50 +0100

    Change healthcheck endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do # rubocop:disable Metrics/BlockLength
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get 'ping' => 'rails/health#show', as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   root 'home#index'
 
